d91e9fa3aa49bb7f9fb2784dc8db2f23d1376264
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-07-13 18:38:17 -0600

    protyping mesh.h

--- src/mesh.h
+++ src/mesh.h
@@ -5,4 +5,30 @@
 #include "vector.h"
 #include "file_finder.h"
 
+// a face triangle stores 3 vertex indices 
+typedef struct{
+
+    int a; 
+    int b; 
+    int c; 
+
+}Face;
+
+// a Mesh stores all vertieces and faces from the obj file 
+
+typedef struct{
+
+    Vec3 *vertices; // array of vertex 
+    Face *face; // array of faces
+    int vertex_count; 
+    int face_count; 
+
+}Mesh;
+
+// loading a mesh from an obj file 
+
+Mesh LoadMeshFromOBJ(const char *filename);
+
+
+
 #endif // MESH_H 
