87215797ff826a9cf078689b0847c29e0206df3a
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-07-13 19:17:59 -0600

    mesh.h neeeds a free mesh fuction

--- src/mesh.h
+++ src/mesh.h
@@ -5,6 +5,8 @@
 #include "vector.h"
 #include "file_finder.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // a face triangle stores 3 vertex indices 
 typedef struct{
@@ -21,14 +23,22 @@ typedef struct{
 
     Vec3 *vertices; // array of vertex 
     Face *face; // array of faces
-    int vertex_count; 
-    int face_count; 
+    Vec3 *Normal_Vec; 
+    Vec2 *texture;
+    int Num_vertex; 
+    int Num_face; 
+    int Num_Nvec;
+    int Num_texture;
 
 }Mesh;
 
-// loading a mesh from an obj file 
 
+void FreeMesh(Mesh model); 
+
+
+// loading a mesh from an obj file 
 Mesh LoadMeshFromOBJ(const char *filename);
+
 
 
 
