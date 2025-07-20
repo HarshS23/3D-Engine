7dd9c575297ba367edac12c70e04606227f7f9a7
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-07-20 15:57:50 -0600

    changed to circle

--- src/display.h
+++ src/display.h
@@ -9,6 +9,8 @@
 int initlizeWindow(void);
 void init_display(int width, int height); 
 void clear_display(void);
+void DrawCircle(SDL_Renderer *Renderer, int x, int y, int r);
+
 void RenderWireFrame(const Mesh *model);
 void RenderWireVertrix(const Mesh *model);
 void present_display(void); 
