b65fe1dab2ce898a5a446163705bc15e9a1b3843
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-07-22 10:55:30 -0600

    made The RotateYawPitch function

--- src/camera.c
+++ src/camera.c
@@ -1,5 +1,30 @@
 #include "camera.h"
 
 Vec3 RotateYawPitch(Vec3 v, float yaw, float pitch){
-    void;
+
+    // apply rotation around y axis 
+    float CosYaw = cosf(yaw);
+    float SinYaw = sinf(yaw); 
+
+    float x1 = v.x * CosYaw - v.z * SinYaw;
+    float z1 = v.x * SinYaw - v.z * CosYaw; 
+    float y1 = v.y;
+
+    // apply pitching rotation around x axis 
+    float CosPitch = cosf(pitch);
+    float SinPitch = sinf(pitch);
+
+    float y2 = y1 * CosPitch - z1 * SinPitch; 
+    float z2 = y1 * SinPitch + z1 * CosPitch; 
+    float x2 = x1; 
+
+    Vec3 rotated; 
+    rotated.x = x2; 
+    rotated.y = y2; 
+    rotated.z = z2; 
+
+    return rotated;
+
+
+
 }
