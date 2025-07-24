67bc747a70f114d8200757629f86011f692bd2ef
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-07-23 23:41:47 -0600

    made a reset feature

--- src/main.c
+++ src/main.c
@@ -65,43 +65,17 @@ int main(){
             if(cam.pitch < -1.5f) cam.pitch = -1.5f;
 
             if(event.type == SDL_KEYDOWN){
+                // render modes
                 if(event.key.keysym.sym == SDLK_1) render_mode = 1; 
                 if(event.key.keysym.sym == SDLK_2) render_mode = 2;
 
-
-
-                // // camera movement 
-                // Vec3 forward = GetForwardVector(cam);
-                // Vec3 right = GetRightVector(cam);
-
-
-                // if(event.key.keysym.sym == SDLK_w) cam.postion = addVec3(cam.postion, scalVec3(forward, 0.1f)); // forwards
-                // if(event.key.keysym.sym == SDLK_s) cam.postion = subVec3(cam.postion, scalVec3(forward, 0.1f)); // backwards 
-                // if(event.key.keysym.sym == SDLK_a) cam.postion = subVec3(cam.postion, scalVec3(right, 0.1f)); // left 
-                // if(event.key.keysym.sym == SDLK_d) cam.postion = addVec3(cam.postion, scalVec3(right, 0.1f)); // right 
-
-                // // up / down vertical movements  (NOT PITCH)
-                // if(event.key.keysym.sym == SDLK_q) cam.postion.y += 0.1f; // move camera upwardes 
-                // if(event.key.keysym.sym == SDLK_e) cam.postion.y -= 0.1f; // more camera downwards 
-
-
-                // if(event.key.keysym.sym == SDLK_LEFT) cam.yaw -= 0.1f; // rotate left 
-                // if(event.key.keysym.sym == SDLK_RIGHT) cam.yaw += 0.1f; // rotate right 
-
-
-                // if(event.key.keysym.sym == SDLK_UP) cam.pitch += 0.1f; // rotate up 
-                // if(event.key.keysym.sym == SDLK_DOWN) cam.pitch -= 0.1f; // rotate down 
-
-                // if(cam.pitch > 1.5f) cam.pitch = 1.5f; 
-                // if(cam.pitch < -1.5f) cam.pitch = -1.5f;
+                // reset option
+                if(event.key.keysym.sym == SDLK_r){
+                    cam.postion = (Vec3){0.0f, 0.0f, 1.5f}; 
+                    cam.yaw = 0; 
+                    cam.pitch = 0;
+                }
             }
-
-
-
-
-
-
-
 
         }
 
