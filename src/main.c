3e11bc39396c9d92356aeea0a77fd7c2a4648f7a
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-07-23 23:35:16 -0600

    made keyboard inputs much smoother

--- src/main.c
+++ src/main.c
@@ -38,6 +38,31 @@ int main(){
             if(event.type == SDL_QUIT){
                 running = 0; 
             }
+            
+            const Uint8 *keys = SDL_GetKeyboardState(NULL);
+             // camera movement 
+            Vec3 forward = GetForwardVector(cam);
+            Vec3 right = GetRightVector(cam);
+
+
+            if(keys[SDL_SCANCODE_W]) cam.postion = addVec3(cam.postion, scalVec3(forward, 0.05f)); // forwards
+            if(keys[SDL_SCANCODE_S]) cam.postion = subVec3(cam.postion, scalVec3(forward, 0.05f)); // backwards 
+            if(keys[SDL_SCANCODE_A]) cam.postion = subVec3(cam.postion, scalVec3(right, 0.05f)); // left 
+            if(keys[SDL_SCANCODE_D]) cam.postion = addVec3(cam.postion, scalVec3(right, 0.05f)); // right 
+
+            // up / down vertical movements  (NOT PITCH)
+            if(keys[SDL_SCANCODE_Q]) cam.postion.y += 0.05f; // move camera upwardes 
+            if(keys[SDL_SCANCODE_E]) cam.postion.y -= 0.05f; // more camera downwards 
+
+
+            if(keys[SDL_SCANCODE_LEFT]) cam.yaw -= 0.03f; // rotate left 
+            if(keys[SDL_SCANCODE_RIGHT]) cam.yaw += 0.03f; // rotate right 
+
+            if(keys[SDL_SCANCODE_UP]) cam.pitch += 0.03f; // rotate up 
+            if(keys[SDL_SCANCODE_DOWN]) cam.pitch -= 0.03f; // rotate down 
+
+            if(cam.pitch > 1.5f) cam.pitch = 1.5f; 
+            if(cam.pitch < -1.5f) cam.pitch = -1.5f;
 
             if(event.type == SDL_KEYDOWN){
                 if(event.key.keysym.sym == SDLK_1) render_mode = 1; 
@@ -45,31 +70,39 @@ int main(){
 
 
 
-                // camera movement 
-                Vec3 forward = GetForwardVector(cam);
-                Vec3 right = GetRightVector(cam);
+                // // camera movement 
+                // Vec3 forward = GetForwardVector(cam);
+                // Vec3 right = GetRightVector(cam);
 
 
-                if(event.key.keysym.sym == SDLK_w) cam.postion = addVec3(cam.postion, scalVec3(forward, 0.1f)); // forwards
-                if(event.key.keysym.sym == SDLK_s) cam.postion = subVec3(cam.postion, scalVec3(forward, 0.1f)); // backwards 
-                if(event.key.keysym.sym == SDLK_a) cam.postion = subVec3(cam.postion, scalVec3(right, 0.1f)); // left 
-                if(event.key.keysym.sym == SDLK_d) cam.postion = addVec3(cam.postion, scalVec3(right, 0.1f)); // right 
+                // if(event.key.keysym.sym == SDLK_w) cam.postion = addVec3(cam.postion, scalVec3(forward, 0.1f)); // forwards
+                // if(event.key.keysym.sym == SDLK_s) cam.postion = subVec3(cam.postion, scalVec3(forward, 0.1f)); // backwards 
+                // if(event.key.keysym.sym == SDLK_a) cam.postion = subVec3(cam.postion, scalVec3(right, 0.1f)); // left 
+                // if(event.key.keysym.sym == SDLK_d) cam.postion = addVec3(cam.postion, scalVec3(right, 0.1f)); // right 
 
-                // up / down vertical movements 
-                if(event.key.keysym.sym == SDLK_q) cam.postion.y += 0.1f;
-                if(event.key.keysym.sym == SDLK_e) cam.postion.y -= 0.1f;
-
-
-                if(event.key.keysym.sym == SDLK_LEFT) cam.yaw -= 0.1f; // rotate left 
-                if(event.key.keysym.sym == SDLK_RIGHT) cam.yaw += 0.1f; // rotate right 
+                // // up / down vertical movements  (NOT PITCH)
+                // if(event.key.keysym.sym == SDLK_q) cam.postion.y += 0.1f; // move camera upwardes 
+                // if(event.key.keysym.sym == SDLK_e) cam.postion.y -= 0.1f; // more camera downwards 
 
 
-                if(event.key.keysym.sym == SDLK_UP) cam.pitch += 0.1f; // rotate up 
-                if(event.key.keysym.sym == SDLK_DOWN) cam.pitch -= 0.1f; // rotate down 
+                // if(event.key.keysym.sym == SDLK_LEFT) cam.yaw -= 0.1f; // rotate left 
+                // if(event.key.keysym.sym == SDLK_RIGHT) cam.yaw += 0.1f; // rotate right 
 
-                if(cam.pitch > 1.5f) cam.pitch = 1.5f; 
-                if(cam.pitch < -1.5f) cam.pitch = -1.5f;
+
+                // if(event.key.keysym.sym == SDLK_UP) cam.pitch += 0.1f; // rotate up 
+                // if(event.key.keysym.sym == SDLK_DOWN) cam.pitch -= 0.1f; // rotate down 
+
+                // if(cam.pitch > 1.5f) cam.pitch = 1.5f; 
+                // if(cam.pitch < -1.5f) cam.pitch = -1.5f;
             }
+
+
+
+
+
+
+
+
         }
 
         clear_display();
