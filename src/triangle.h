bf9908311037e19e39f1dd8bf2debdfd2a2f9580
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-08-01 10:30:19 -0600

    made a renderfilled with vertices mode, still in progress but almost finished

--- src/triangle.h
+++ src/triangle.h
@@ -7,6 +7,6 @@
 
 void filledTriangle(SDL_Renderer *renderer, int x0, int y0, int x1, int y1, int x2 , int y2);
 
-
+void filledTriangleVertex(SDL_Renderer *renderer, int x0, int y0, int x1, int y1, int x2 , int y2);
 
 #endif
