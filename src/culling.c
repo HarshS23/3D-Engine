ea5ebb00e0108adfb5d08f2ea8ee7c92cb9a9320
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-08-25 17:36:34 -0600

    culling has been fixed and lighting is still buggy

--- src/culling.c
+++ src/culling.c
@@ -4,9 +4,15 @@
 int FaceVisible(Vec3 v0, Vec3 v1, Vec3 v2){
     Vec3 edge1 = subVec3(v1, v0);
     Vec3 edge2 = subVec3(v2, v0);
-    Vec3 normal = normalizeVec3(crossVec3(edge1, edge2));
     
-    Vec3 view_direc = {0,0,1}; 
-    return dotVec3(normal , view_direc) > 0.0f ;
+    Vec3 normal = normalizeVec3(crossVec3(edge2, edge1));
+    
+    Vec3 view_direc = {0.0f,0.0f,-1.0f}; 
+
+    float dot = dotVec3(normal, view_direc);
+    printf("Face dot: %.2f\n", dot);
+
+    return dot < 0.0f;
+    //return dotVec3(normal , view_direc) < 0.0f ;
 
 }
