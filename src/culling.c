a7f0a2144d8593d1d915ca73d9f80049cfc432fb
adharsh <Harsh.shokka@gmail.com>
2025-10-17 12:41:45 -0600

    updated movement speed

--- src/culling.c
+++ src/culling.c
@@ -6,12 +6,13 @@ int FaceVisible(Vec3 v0, Vec3 v1, Vec3 v2){
     Vec3 edge2 = subVec3(v2, v0);
     
     Vec3 normal = normalizeVec3(crossVec3(edge2, edge1));
-    
+
+
     Vec3 view_direc = {0.0f,0.0f,1.0f}; 
 
     float dot = dotVec3(normal, view_direc);
 
-    return dot > 0.0f;
+    return dot > -0.0f;
 
 }
 
